15c51e9ead8d88677e257c9f5fde293c8d7da8eb
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2025-10-31 23:53:26 +0100

    Added saving function
    Introduced save_to_csv function in order to save the trajectory and use it for plotting later to make sure the conservation of energy is not violated by the Euler method

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
+use std::fs::File;
+use std::io::{Result, Write};
 use std::vec::Vec;
 
 const G: f32 = 6.67408e-11; // m3 kg-1 s-2
 
-fn main() {
+fn main() -> Result<()> {
     let mut bodies = vec![
         Body::new(5.0e24, [0.0, 0.0], [0.0, 0.0]), // Earth
         Body::new(7.35e22, [384400000.0, 0.0], [0.0, 1022.0]), // Moon
     ];
 
-    let dt = 60.0; // timestep in seconds
-    for _ in 0..1000 {
+    let steps = 500000;
+    let dt = 10.0;
+
+    let mut history: Vec<Vec<[f32; 2]>> = vec![Vec::with_capacity(steps); bodies.len()];
+    let mut time: Vec<f32> = Vec::with_capacity(steps);
+
+    for step in 0..steps {
         let forces = super_position(&bodies);
         integral(&mut bodies, &forces, dt);
+
+        for (i, body) in bodies.iter().enumerate() {
+            history[i].push(body.position);
+        }
+
+        time.push(step as f32 * dt);
     }
 
-    for (i, b) in bodies.iter().enumerate() {
-        println!(
-            "Body {} -> Position: {:?}, Velocity: {:?}",
-            i, b.position, b.velocity
-        );
+    for (i, traj) in history.iter().enumerate() {
+        let filename = format!("body_{}.csv", i);
+        save_to_csv(&time, traj, &filename)?;
     }
+
+    Ok(())
+}
+
+fn save_to_csv(t: &[f32], traj: &[[f32; 2]], path: &str) -> Result<()> {
+    let mut file = File::create(path)?;
+    writeln!(file, "t,x,y")?;
+    for (i, &time) in t.iter().enumerate() {
+        let [x, y] = traj[i];
+        writeln!(file, "{:.5},{:.5},{:.5}", time, x, y)?;
+    }
+    Ok(())
 }
 
 #[derive(Debug, Clone)]
@@ -43,7 +66,7 @@ impl Body {
         let dx = other_body.position[0] - self.position[0];
         let dy = other_body.position[1] - self.position[1];
 
-        let dr2 = dx * dx + dy * dy;
+        let dr2 = dx * dx + dy * dy + 1e-10;
         let dr = dr2.sqrt();
 
         let force = G * self.mass * other_body.mass / dr2;
@@ -69,10 +92,10 @@ fn super_position(bodies: &[Body]) -> Vec<[f32; 2]> {
 
 fn integral(bodies: &mut [Body], forces: &[[f32; 2]], dt: f32) {
     for (body, force) in bodies.iter_mut().zip(forces.iter()) {
-        let accelation = [force[0] / body.mass, force[1] / body.mass];
+        let acceleration = [force[0] / body.mass, force[1] / body.mass];
 
-        body.velocity[0] += accelation[0] * dt;
-        body.velocity[1] += accelation[1] * dt;
+        body.velocity[0] += acceleration[0] * dt;
+        body.velocity[1] += acceleration[1] * dt;
 
         body.position[0] += body.velocity[0] * dt;
         body.position[1] += body.velocity[1] * dt;
